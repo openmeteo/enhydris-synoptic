5d353d7824138c14a21aee01a85174da37fc07ec
Antonis Christofides <antonis@antonischristofides.com>
2019-07-04 13:56:24 +0300

    Remove links to change related station in inline (fixes #13)

--- enhydris_synoptic/admin.py
+++ enhydris_synoptic/admin.py
@@ -7,7 +7,18 @@ from enhydris_synoptic.models import SynopticGroup, SynopticGroupStation
 
 class StationInline(admin.TabularInline):
     model = SynopticGroup.stations.through
-    show_change_link = True
+
+    def get_formset(self, request, obj=None, **kwargs):
+        """
+        Override the get_formset method to remove the add/change buttons beside the
+        foreign key pull-down menus in the inline.
+        """
+        formset = super().get_formset(request, obj, **kwargs)
+        form = formset.form
+        widget = form.base_fields["station"].widget
+        widget.can_add_related = False
+        widget.can_change_related = False
+        return formset
 
 
 @admin.register(SynopticGroup)
