7a71579a3c6c7fa4fb7fe1919a4e9fa093d9fb6e
Antonis Christofides <antonis@antonischristofides.com>
2020-12-15 15:12:59 +0200

    Use headless Chrome instead of PhantomJS in Travis

--- .travis-settings.py
+++ .travis-settings.py
@@ -1,3 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
 from . import *  # NOQA
 
 DATABASES = {
@@ -13,3 +16,13 @@ DATABASES = {
 
 INSTALLED_APPS.append("enhydris_synoptic")  # NOQA
 ENHYDRIS_SYNOPTIC_ROOT = "/tmp/enhydris-synoptic-root"
+
+headless = ChromeOptions()
+headless.add_argument("--headless")
+SELENIUM_WEBDRIVERS = {
+    "default": {
+        "callable": webdriver.Chrome,
+        "args": [],
+        "kwargs": {"options": headless},
+    },
+}
